9f066b7df0cb8b2a2f961c021061f619831b99c9
George Kampanos <george.kampanos@nandos.co.uk>
2023-07-09 20:24:48 +0100

    :grapes: added some defaults

---
+++ lua/grape.lua
@@ -0,0 +1,21 @@
+-- Name:         Grape
+-- Description:  A dark purple theme
+-- Author:       kampanosg <kampanosg@outlook.com>
+-- Maintainer:   kampanosg <kampanosg@outlook.com>
+-- Website:      httpc.//github.com/kampanosg/grape.nvim
+-- License:      MIT
+
+local Color, c, Group, g, s = require("colorbuddy").setup()
+local b = s.bold
+local i = s.italic
+local n = s.inverse
+local uc = s.undercurl
+local ul = s.underline
+local r = s.reverse
+local sto = s.standout
+local no = s.NONE
+local v = vim
+
+v.g.colors_name = 'grape'
+
+
